a7d0373cdc35b7b707caf34ee3f6e17cd977a86f
Stephen Chen <g1222888@gmail.com>
2020-01-26 17:06:04 +0800

    update metadata

--- setup.py
+++ setup.py
@@ -5,21 +5,22 @@ from setuptools import setup, find_packages
 
 readme = open('README.md', 'r').read()
 setup(
-    name='gists.cli',
+    name='gists.cli.py3',
     version='0.351',
-    url='https://github.com/khilnani/gists.cli',
+    url='https://github.com/RPing/gists.cli',
     license='GPLv2',
-    author='khilnani',
-    author_email='nik@khilnani.org',
-    description='A CLI to create, edit, view and list your GitHub Gists',
+    author='rping',
+    author_email='g1222888@gmail.com',
+    description='A CLI to create, edit, view and list your GitHub Gists, forked from https://github.com/jiahut/gists.cli',
     include_package_data=True,
     long_description=readme,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=['requests','texttable'],
     entry_points={
         'console_scripts': [
             'gists = gists_cli.gists:main',
-            ]
+        ]
     },
     classifiers=[
           'Development Status :: 2 - Pre-Alpha',
